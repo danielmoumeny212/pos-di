0f18c6251d7f5fe718bc31a701fc9f3e83ab2e28
Daniel Moumeny <107214389+danielmoumeny212@users.noreply.github.com>
2024-06-20 11:52:05 +0200

    feat:setup-create-table-convex

---
+++ convex/schema.ts
@@ -0,0 +1,85 @@
+import { defineTable, defineSchema } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  users: defineTable({
+    username: v.string(),
+    password: v.string(),
+    role: v.string(),
+  }),
+
+  // Table for Teamv
+  //   teams: defineTable({
+  //     revtaurantId: v.string(),
+  //     role: v.string()
+  //   }),
+
+  // Table for Restaurants
+  restaurants: defineTable({
+    name: v.string(),
+    address: v.string(),
+    contactDetails: v.string(),
+    restaurantId: v.id("users"),
+  }),
+
+  // Table for Menus
+  menus: defineTable({
+    restaurantId: v.id("restaurants"),
+    name: v.string(),
+  }),
+
+  menu_category: defineTable({
+    menuId: v.id("menus"),
+    name: v.string(),
+  }),
+
+  // Table for Menu Items
+  menu_items: defineTable({
+    id: v.id("menu_category"),
+    menuId: v.string(),
+    name: v.string(),
+  }),
+
+  products: defineTable({
+    categoryId: v.id("menu_category"),
+    name: v.string(),
+    description: v.string(),
+    price: v.number(),
+    
+    customizableOption: v.string(),
+  }),
+
+  // Table for Orders
+  orders: defineTable({
+    restaurantId: v.id("restaurants"),
+    customerId: v.id("customers"),
+    orderNumber: v.string(),
+    status: v.string(),
+  }),
+
+  // Table for Order Detailv
+  order_details: defineTable({
+    orderId: v.id("orders"),
+    productId: v.id("products"),
+    quantity: v.number(),
+    options: v.string(),
+  }),
+
+  // Table for Customer
+  customers: defineTable({
+    name: v.string(),
+    phoneNumber: v.number(),  
+    contactDetails: v.string(),
+  }),
+
+  kds: defineTable({
+    orderId: v.id("orders"),
+    details: v.string(),
+  }),
+
+  // Table for Point of vale (POv)
+  pos: defineTable({
+    orderId: v.id("orders"),
+    details: v.string(),
+  }),
+});
